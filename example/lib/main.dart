772767f480ceb6164aaabee68eea441d037a5c10
Greg <gregory.reiser@yahoo.fr>
2024-12-28 20:02:43 -0500

    WIP

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,26 +1,9 @@
 import 'package:crisp_chat/crisp_chat.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
-import 'firebase_options.dart';
-
-@pragma('vm:entry-point')
-Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
-  await Firebase.initializeApp();
-
-  if (kDebugMode) {
-    print("Handling a background message: ${message.messageId}");
-  }
-}
-
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
   runApp(const MyApp());
 }
 
@@ -91,8 +74,7 @@ class _MyAppState extends State<MyApp> {
 
                   /// Checking session ID After 5 sec
                   await Future.delayed(const Duration(seconds: 5), () async {
-                    String? sessionId =
-                        await FlutterCrispChat.getSessionIdentifier();
+                    String? sessionId = await FlutterCrispChat.getSessionIdentifier();
                     if (sessionId != null) {
                       if (kDebugMode) {
                         print('Session ID: $sessionId');
